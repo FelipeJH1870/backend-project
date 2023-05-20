6f42d92d98a031a331a7b93723af15b21dbe9d2a
Fjh-gith777 <102310742+Fjh-gith777@users.noreply.github.com>
2023-05-19 21:20:53 -0600

    first commit

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ import express from 'express';
 
 const app = express();
 
+/* 
+app.get('/', (req, res) => {
+  res.send("Hello, world!");
+});
+ */
+app.use(express.static('public'));
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT);
